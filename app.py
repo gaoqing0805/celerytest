0d1115edbf654a2ea4616824052147c3255561d3
gaoqing0805 <972598953@qq.com>
2019-07-27 00:00:50 +0800

    celery demo

---
+++ app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Name: app.py
+Author: cyan
+Time: 2019/7/26 10:36
+Desc: 
+"""
+import time
+from tasks import add
+
+if __name__ == '__main__':
+    print 'start task...'
+    result = add.delay(3, 5)
+    print 'end task...'
+    print result
---
+++ readme.md
@@ -0,0 +1,8 @@
+
+1 celery创建worker
+
+celery worker -A tasks -l INFO
+
+2 pip list 查看已安装的库
+
+3
